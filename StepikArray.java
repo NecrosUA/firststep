601b684cc669b433d924a7fe5dc3c582f4de91ef
Rostislav <Rostislav.email@gmail.com>
2016-10-24 12:37:19 +0300

    Sorting 2 Arrays
    Поєднання та сортування двох різних по величині масивів...

--- StepikArray.java
+++ StepikArray.java
@@ -8,10 +8,10 @@ public class StepikArray {
     public static void main(String[] args) {
         int[] a1 = {0, 2, 2};
         int[] a2 = {1, 3};
-        int[] a3 = new int[a1.length + a2.length];
-        System.arraycopy(a1, 0, a3, 0, a1.length);
-        System.arraycopy(a2, 0, a3, a1.length, a2.length);
-        for (int i = 0; i < a3.length; i++) {
+        int[] a3 = new int[a1.length + a2.length]; // a3 length = length a1 + a2 length
+        System.arraycopy(a1, 0, a3, 0, a1.length); // Copy Array a1 to a3
+        System.arraycopy(a2, 0, a3, a1.length, a2.length); // Copy Array a2 to a3
+        for (int i = 0; i < a3.length; i++) { //Cycle for sorting 
 
             int min = a3[i];
             int imin = i; 
@@ -30,6 +30,6 @@ public class StepikArray {
                 a3[imin] = temp;
             }
         }
-        System.out.println(Arrays.toString(a3));
+        System.out.println(Arrays.toString(a3)); //Prints Array 
     }
 }
