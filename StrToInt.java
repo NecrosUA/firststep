ccb6429557bfd7ced874face14ae7ee17f284c70
Rostislav <Rostislav.email@gmail.com>
2016-10-27 15:40:53 +0300

    Now its searching value closes to 0

--- StrToInt.java
+++ StrToInt.java
@@ -9,13 +9,20 @@ import java.util.stream.Stream;
 
 public class StrToInt {
     public static void main(String[] args) {
-        String temps = "2 -3 15 10 8";
+        String temps = "2 -3 15 10 8 1";
 
         int[] array = Stream.of(temps.split(" ")).mapToInt(Integer::parseInt).toArray();
         Arrays.sort(array);
+        int near = array[0];
+
+        for (int i = 0; i < array.length; i++){
+                if (Math.abs(array[i]) < Math.abs(near)){
+                    near = array[i];
+                }
+        }
 
 
-
-        System.out.println(Arrays.toString(array));
+          System.out.println(near);
+          System.out.println(Arrays.toString(array));
     }
 }
